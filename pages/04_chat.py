67a5a7b9dfd59a9fe971c9deeaa157e0ae05acea
MEDA <77627425+adem8293@users.noreply.github.com>
2024-11-21 07:19:36 +0000

    Pending changes exported from your codespace

--- pages/04_chat.py
+++ pages/04_chat.py
@@ -1,15 +1,32 @@
 import streamlit as st
+import json
+from lib.tools import generate_image, SCHEMA_GENERATE_IMAGE
 
-@st.cache_data
-def generate_image(prompt):
-    client = st.session_state['openai_client']
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=prompt,
-        n=1,
-        size="1024x1024"
-    )
-    return response.data[0].url
+TOOL_FUNCTIONS = {
+    "generate_image": generate_image
+}
+
+FUNCTION_TOOLS_SCHEMA = [
+    SCHEMA_GENERATE_IMAGE
+]
+
+def show_message(msg):
+    if msg['role'] == 'user' or msg['role'] == 'assistant':
+        with st.chat_message(msg['role']):
+            st.markdown(msg["content"])
+    elif msg['role'] == 'code':
+        with st.chat_message('assistant'):
+            with st.expander("Show codes"):
+                st.code(msg["content"], language='python')
+    elif msg['role'] == 'image_url':
+        with st.chat_message('assistant'):
+            st.markdown(f"![]({msg['content']})")
+    elif msg['role'] == 'image_file':
+        with st.chat_message('assistant'):
+            st.image(msg['content'])
+
+
+# Initialization
 
 client = st.session_state.get('openai_client', None)
 if client is None:
@@ -24,39 +41,39 @@ if "assistant" not in st.session_state:
     st.session_state.assistant = client.beta.assistants.create(
         name="Assistant",
         model="gpt-4o-mini",
-        tools=[
-            {"type":"code_interpreter"}
-        ]
+        tools=[{"type":"code_interpreter"}] + FUNCTION_TOOLS_SCHEMA
     )
 
 if "thread" not in st.session_state:
-    thread = client.beta.threads.create()
+    st.session_state.thread = client.beta.threads.create()
 
+
+# Page
 
 st.header("Chat")
 
 col1, col2 = st.columns(2)
 with col1:
     if st.button("Clear (Start a new chat)"):
-        del st.session_state.messages
+        st.session_state.messages = []
         del st.session_state.thread
 with col2:
     if st.button("Leave"):
-        del st.session_state.messages
+        st.session_state.messages = []
         del st.session_state.thread
         del st.session_state.assistant
 
 # previous chat
 for msg in st.session_state.messages:
-    with st.chat_message(msg['role']):
-        st.markdown(msg["content"])
+    show_message(msg)
 
 # user prompt, assistant response
 if prompt := st.chat_input("What is up?"):
-    st.chat_message("user").markdown(prompt)
-    st.session_state.messages.append({"role":"user", "content":prompt})
+    msg = {"role":"user", "content":prompt}
+    show_message(msg)
+    st.session_state.messages.append(msg)
 
-    # assistant api
+    # assistant api - get response
     thread = st.session_state.thread
     assistant = st.session_state.assistant
 
@@ -69,15 +86,62 @@ if prompt := st.chat_input("What is up?"):
         thread_id=thread.id,
         assistant_id=assistant.id
     )
+
+    while run.status == 'requires_action':
+        tool_calls = run.required_action.submit_tool_outputs.tool_calls
+        tool_outputs = []
+        for tool in tool_calls:
+            func_name = tool.function.name
+            kwargs = json.loads(tool.function.arguments)
+            output = None
+            if func_name in TOOL_FUNCTIONS:
+                output = TOOL_FUNCTIONS[func_name](**kwargs)
+            tool_outputs.append(
+                {
+                    "tool_call_id": tool.id,
+                    "output": str(output)
+                }
+            )
+        run = client.beta.threads.runs.submit_tool_outputs_and_poll(
+            thread_id=thread.id,
+            run_id=run.id,
+            tool_outputs=tool_outputs
+        )
+            
+    # assistant messages - text, image_url, image_file
     if run.status == 'completed':
         api_response = client.beta.threads.messages.list(
             thread_id=thread.id,
             run_id=run.id,
-            limit=1
+            order="asc"
         )
+        for data in api_response.data:
+            for content in data.content:
+                if content.type == 'text':
+                    response = content.text.value
+                    msg = {"role":"assistant","content":response}
+                elif content.type == 'image_url':
+                    url = content.image_url.url
+                    msg = {"role":"image_url","content":url}
+                elif content.type == 'image_file':
+                    file_id = content.image_file.file_id
+                    # load file
+                    image_data = client.files.content(file_id)
+                    msg = {"role":"image_file","content":image_data.read()}
+                show_message(msg)
+                st.session_state.messages.append(msg)
 
-    response = api_response.data[0].content[0].text.value
-
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    st.session_state.messages.append({"role":"assistant","content":response})
+    # code interpreter tool call info
+    run_steps = client.beta.threads.runs.steps.list(
+        thread_id=thread.id,
+        run_id=run.id,
+        order='asc'
+    )
+    for run_step in run_steps.data:
+        if run_step.step_details.type == 'tool_calls':
+            for tool_call in run_step.step_details.tool_calls:
+                if tool_call.type == 'code_interpreter':
+                    code = tool_call.code_interpreter.input
+                    msg = {"role":"code","content":code}
+                    show_message(msg)
+                    st.session_state.messages.append(msg)
